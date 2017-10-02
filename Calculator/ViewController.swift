515053739bda5cb39dc703995b98f2350b739032
Celia Gómez de Villavedón Pedrosa <celiagomezdev@gmail.com>
2017-10-02 17:38:18 +0200

    Set UI

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,15 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
