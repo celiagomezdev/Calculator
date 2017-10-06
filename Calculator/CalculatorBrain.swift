0127998876e676055585bfb2687750a047b1c44e
celiagomezdev <celiagomezdev@gmail.com>
2017-10-06 12:20:24 +0200

    Add operations closures

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,15 +8,6 @@
 
 import Foundation
 
-
-func changeSign(operand: Double) -> Double {
-    return -operand
-}
-
-func multiply(op1: Double, op2: Double) -> Double {
-    return op1 * op2
-}
-
 struct CalculatorBrain {
     
     private var accumulator: Double?
@@ -33,9 +24,12 @@ struct CalculatorBrain {
         "e": Operation.constant(M_E),
         "√": Operation.unaryOperation(sqrt),
         "cos": Operation.unaryOperation(cos),
-        "±": Operation.unaryOperation(changeSign),
-        "×": Operation.binaryOperation(multiply),
-        "=": Operation.equals
+        "±": Operation.unaryOperation({ -$0 }),
+        "×": Operation.binaryOperation({ $0 * $1 }),
+        "÷": Operation.binaryOperation({ $0 / $1 }),
+        "+": Operation.binaryOperation({ $0 + $1 }),
+        "-": Operation.binaryOperation({ $0 - $1 }),
+        "=": Operation.equals,
         ]
     
     mutating func performOperation(_ symbol: String) {
